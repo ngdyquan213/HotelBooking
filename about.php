e1f06dc81b5ee15d4b32fda51c83492385d43d44
@prosam <ngdyquan213@gmail.com>
2024-08-20 23:52:13 +0700

    about

---
+++ about.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DQ Hotel - ABOUT</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <?php require('inc/links.php'); ?>
+    <style>
+        .box {
+            border-top-color: var(--teal) !important;
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">ABOUT US</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+            Impedit possimus deleniti ipsam, iste assumenda illo maxime minus? <br>
+            Autem, tempore debitis atque fuga excepturi, consequuntur hic maiores pariatur repellat alias quibusdam.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
+                <h3 class="mb-3">Lorem ipsum dolor sit</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur, nunc non sagittis consectetur, velit velit
+                    ultricies velit, at consectetur nisi odio nec velit. Nullam non orci vel massa condimentum vulputate. Nullam
+                    tincidunt, ex vel dignissim tristique, mauris ipsum fermentum massa, a ultricies purus lectus eu massa.
+                    Curabitur ut diam in velit tincidunt congue.
+                </p>
+            </div>
+            <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
+                <img src="images/about/about.jpg" class="w-100">
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/hotel.svg" width="70px">
+                    <h4 class="mt-3">100+ ROOMS</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/customers.svg" width="70px">
+                    <h4 class="mt-3">200+ CUSTOMERS</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/rating.svg" width="70px">
+                    <h4 class="mt-3">150+ REVIEWS</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/about/staff.svg" width="70px">
+                    <h4 class="mt-3">200+ STAFFS</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <h3 class="my-5 fw-bold h-font text-center">MANAGERMENT TEAM</h3>
+
+    <div class="container px-4">
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper mb-5">
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+                <div class="swiper-slide bg-white text-center overflow-hidden rounded">
+                    <img src="images/about/IMG_17352.jpg" class="w-100">
+                    <h5 class="mt-2">Random Name</h5>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
+    <?php require('inc/footer.php'); ?>
+
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 40,
+            pagination: {
+                el: ".swiper-pagination",
+                dynamicBullets: true,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                640: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+
+            }
+        });
+    </script>
+
+</body>
+
+</html>
