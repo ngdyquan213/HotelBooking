d008b845e60f543d63db7be3c86440f780d72524
@prosam <ngdyquan213@gmail.com>
2024-08-29 16:27:34 +0700

    done

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,22 +1,74 @@
 <?php
-    require('inc/essentails.php');
-    adminLogin();
+require('inc/essentails.php');
+adminLogin();
+session_regenerate_id(true);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Dashboard</title>
     <?php require('inc/links.php') ?>
 </head>
+
 <body class="bg-light">
-    
+
     <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
-        <h3 class="mb-0">ADMIN PANEL</h3>
+        <h3 class="mb-0 h-font">DQ Shop</h3>
         <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+    </div>
+
+    <div class="col-lg-2 bg-dark border-top border-3 border-secondary">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid flex-lg-column align-items-stretch">
+                <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+                <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+                    <div class="border bg-light p-3 rounded mb-3">
+                        <h5 class="mb-3" stype="form-size: 18px;">CHECK AVAILABILITY</h5>
+                        <label class="form-label">Check-in</label>
+                        <input type="date" class="form-control shadow-none mb-3">
+                        <label class="form-label">Check-out</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class="border bg-light p-3 rounded mb-3">
+                        <h5 class="mb-3" stype="form-size: 18px;">FACILITIES</h5>
+                        <div class="mb-2">
+                            <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                            <label class="form-label" for="f1">Facility one</label>
+                        </div>
+                        <div class="mb-2">
+                            <input type="checkbox" id="f2" class="form-check-input shadow-none me-1">
+                            <label class="form-label" for="f2">Facility two</label>
+                        </div>
+                        <div class="mb-2">
+                            <input type="checkbox" id="f3" class="form-check-input shadow-none me-1">
+                            <label class="form-label" for="f3">Facility three</label>
+                        </div>
+                    </div>
+                    <div class="border bg-light p-3 rounded mb-3">
+                        <h5 class="mb-3" stype="form-size: 18px;">GUESTS</h5>
+                        <div class="d-flex">
+                            <div class="me-3">
+                                <label class="form-label">Adults</label>
+                                <input type="number" class="form-control shadow-none">
+                            </div>
+                            <div>
+                                <label class="form-label">Children</label>
+                                <input type="number" class="form-control shadow-none">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </div>
 
     <?php require('inc/scripts.php') ?>
 </body>
+
 </html>
