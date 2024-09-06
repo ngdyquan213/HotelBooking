653df9c7a9f771f720e96e56b0dd3c486aa04920
@prosam <ngdyquan213@gmail.com>
2024-09-06 11:22:08 +0700

    Done

--- admin/inc/essentails.php
+++ admin/inc/essentails.php
@@ -3,13 +3,17 @@
     // frontend purpose data
 
     define('SITE_URL','http://127.0.0.1/HotelBooking/');
-    define('ABOUT_IMG_PATH', SITE_URL.'images/about/' );
+    define('ABOUT_IMG_PATH', SITE_URL.'images/about/');
+    define('CAROUSEL_IMG_PATH', SITE_URL.'images/carousel/');
 
 
     // backend upload progress needs this data
 
     define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'].'/HotelBooking/images/');
     define('ABOUT_FOLDER', 'about/');
+    define('CAROUSEL_FOLDER', 'carousel/');
+
+
     function adminLogin() {
         session_start();
         if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)){
