42da84bd9b954cc44ba481a6272d28514a167e99
@prosam <ngdyquan213@gmail.com>
2024-09-09 01:00:59 +0700

    Done

--- admin/ajax/features_facilities.php
+++ admin/ajax/features_facilities.php
@@ -35,11 +35,17 @@
 
     if(isset($_POST['rem_feature'])){
         $frm_data = filteration($_POST);
-        
-        $q = "DELETE FROM `features` WHERE `id`=?";
-        $values = [$frm_data['rem_feature']];
-        $res = detele($q, $values, 'i');
-        echo $res;
+
+        $check_q = select("SELECT * FROM `room_features` WHERE `features_id` =?",[$frm_data['rem_feature']],'i');
+
+        if(mysqli_num_rows($check_q) == 0){
+            $q = "DELETE FROM `features` WHERE `id`=?";
+            $values = [$frm_data['rem_feature']];
+            $res = detele($q, $values, 'i');
+            echo $res;
+        }else{
+            echo 'room_added';
+        }
     }
 
 
@@ -87,18 +93,25 @@
     if(isset($_POST['rem_facility'])){
         $frm_data = filteration($_POST);
 
-        $pre_q = "SELECT * FROM `facilities` WHERE  `id`=?";
-        $values = [$frm_data['rem_facility']];
-        $res = select($pre_q, $values, 'i');
+        $check_q = select("SELECT * FROM `room_facilities` WHERE `facilities_id` =?",[$frm_data['rem_facility']],'i');
 
-        $img = mysqli_fetch_assoc($res);
-        
-        if(deleteImage($img['icon'], FACILITIES_FOLDER)){
-            $q = "DELETE FROM `facilities` WHERE `id`=?";
-            $res = detele($q, $values, 'i');
-            echo $res;
+        if(mysqli_num_rows($check_q) == 0){
+            $pre_q = "SELECT * FROM `facilities` WHERE  `id`=?";
+            $values = [$frm_data['rem_facility']];
+            $res = select($pre_q, $values, 'i');
+    
+            $img = mysqli_fetch_assoc($res);
+            
+            if(deleteImage($img['icon'], FACILITIES_FOLDER)){
+                $q = "DELETE FROM `facilities` WHERE `id`=?";
+                $res = detele($q, $values, 'i');
+                echo $res;
+            }else{
+                echo 0;
+            }
         }else{
-            echo 0;
+            echo 'room_added';
         }
+
 
     }
--- facilities.php
+++ facilities.php
@@ -50,57 +50,6 @@
                     data;
                 }
             ?>
-
-            <!-- <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/facilities/IMG_43553.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-
-                    <p>Free Wi-Fi available throughout the hotel.</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/facilities/IMG_43553.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-
-                    <p>Free Wi-Fi available throughout the hotel.</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/facilities/IMG_43553.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-
-                    <p>Free Wi-Fi available throughout the hotel.</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/facilities/IMG_43553.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-
-                    <p>Free Wi-Fi available throughout the hotel.</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="images/facilities/IMG_43553.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
-
-                    <p>Free Wi-Fi available throughout the hotel.</p>
-                </div>
-            </div> -->
         </div>
     </div>
 
