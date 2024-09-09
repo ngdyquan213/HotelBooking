cd175c173aa4ba917e7577d3c53b4479dd9be69b
@prosam <ngdyquan213@gmail.com>
2024-09-09 21:44:12 +0700

    Doone

--- index.php
+++ index.php
@@ -34,16 +34,16 @@
             <div class="swiper-wrapper">
 
                 <?php
-                    $res = selectAll('carousel');
+                $res = selectAll('carousel');
 
-                    while ($row = mysqli_fetch_assoc($res)){
-                        $path = CAROUSEL_IMG_PATH;
-                        echo <<<data
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $path = CAROUSEL_IMG_PATH;
+                    echo <<<data
                             <div class="swiper-slide">
                                 <img src="$path$row[image]" class="w-100 d-block" />
                             </div>
                         data;
-                    }
+                }
                 ?>
             </div>
         </div>
@@ -94,7 +94,94 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-md-6 my-3">
+            <?php
+                $room_res = select("SELECT * FROM `rooms` WHERE `status`=? AND `removed`=? ORDER BY `id` DESC LIMIT 3", [1, 0], 'ii');
+
+                while ($room_data = mysqli_fetch_assoc($room_res)) {
+
+                    $fea_q = mysqli_query($con, "SELECT f.name FROM `features` f 
+                            INNER JOIN `room_features` rfea ON f.id = rfea.features_id
+                            WHERE rfea.room_id = '$room_data[id]'");
+
+                    $features_data = "";
+
+                    while ($fea_row = mysqli_fetch_assoc($fea_q)) {
+                        $features_data .= "<span class='badge rounded-pill bg-light text-dark text-wrap me-1 mb-1'>
+                                                    $fea_row[name]
+                                                </span>";
+                    }
+
+                    $fac_q = mysqli_query($con, "SELECT f.name FROM `facilities` f
+                            INNER JOIN `room_facilities` rfac ON f.id = rfac.facilities_id
+                            WHERE rfac.room_id = '$room_data[id]'");
+
+                    $facilities_data = "";
+
+                    while ($fac_row = mysqli_fetch_assoc($fac_q)) {
+
+                        $facilities_data .= "<span class='badge rounded-pill bg-light text-dark text-wrap me-1 mb-1'>
+                                                        $fac_row[name]
+                                                    </span>";
+                    }
+
+                    $room_thumb = ROOMS_IMG_PATH . "thumbnail.jpg";
+                    $thumb_q = mysqli_query($con, "SELECT * FROM `room_images`
+                            WHERE `room_id` = '$room_data[id]'
+                            AND `thumb`='1'");
+
+                    if (mysqli_num_rows($thumb_q) > 0) {
+                        $thumb_res = mysqli_fetch_assoc($thumb_q);
+                        $room_thumb = ROOMS_IMG_PATH . $thumb_res['image'];
+                    }
+
+
+                    echo <<<data
+                        <div class="col-lg-4 col-md-6 my-3">
+                            <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+                                <img src="$room_thumb" class="card-img-top">
+                                <div class="card-body">
+                                    <h5>$room_data[name]</h5>
+                                    <h6 class="mb-4">$room_data[price] per night</h6>
+                                    <div class="features mb-4">
+                                        <h6 class="mb-1">Features</h6>
+                                        $features_data
+                                    </div>
+                                    <div class="facilities mb-4">
+                                        <h6 class="mb-1">Facilities</h6>
+                                        $facilities_data
+                                    </div>
+                                    <div class="guest mb-4">
+                                        <h6 class="mb-1">Guests</h6>
+                                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                            $room_data[adult] Adults
+                                        </span>
+                                        <span class="badge rounded-pill bg-light text-dark text-wrap">
+                                            $room_data[children] Children
+                                        </span>
+                                    </div>
+                                    <div class="rating mb-4">
+                                        <h6 class="mb-1">Rating</h6>
+                                        <span class="badge rounded-pill bg-light">
+                                            <i class="bi bi-star-fill text-warning"></i>
+                                            <i class="bi bi-star-fill text-warning"></i>
+                                            <i class="bi bi-star-fill text-warning"></i>
+                                            <i class="bi bi-star-fill text-warning"></i>
+                                            <i class="bi bi-star-fill text-warning"></i>
+                                        </span>
+                                    </div>
+                                    <div class="d-flex justify-content-evenly mb-2">
+                                        <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+                                        <a href="room_details.php?id=$room_data[id]" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    data;
+                }
+            ?>
+
+
+            <!-- <div class="col-lg-4 col-md-6 my-3">
                 <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
                     <img src="images/rooms/1.jpg" class="card-img-top">
                     <div class="card-body">
@@ -281,10 +368,10 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
             <div class="col-lg-12 text-center mt-5">
-                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms >>></a>
+                <a href="rooms.php" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms >>></a>
             </div>
         </div>
     </div>
@@ -294,28 +381,21 @@
 
     <div class="container">
         <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
-            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="images/facilities/wifi.svg" width="80px">
-                <h5 class="mt-3">Wifi</h5>
-            </div>
-            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="images/facilities/wifi.svg" width="80px">
-                <h5 class="mt-3">Wifi</h5>
-            </div>
-            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="images/facilities/wifi.svg" width="80px">
-                <h5 class="mt-3">Wifi</h5>
-            </div>
-            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="images/facilities/wifi.svg" width="80px">
-                <h5 class="mt-3">Wifi</h5>
-            </div>
-            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="images/facilities/wifi.svg" width="80px">
-                <h5 class="mt-3">Wifi</h5>
-            </div>
+            <?php
+            $res = mysqli_query($con, "SELECT * FROM `facilities` ORDER BY `id` DESC LIMIT 5");
+            $path = FACILITIES_IMG_PATH;
+
+            while ($row = mysqli_fetch_assoc($res)) {
+                echo <<<data
+                        <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+                            <img src="$path$row[icon]" width="60px">
+                            <h5 class="mt-3">$row[name]</h5>
+                        </div>
+                    data;
+            }
+            ?>
             <div class="col-lg-12 text-center mt-5">
-                <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+                <a href="facilities.php" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
             </div>
         </div>
     </div>
@@ -398,7 +478,7 @@
             <div class="swiper-pagination"></div>
         </div>
         <div class="col-lg-12 text-center mt-5">
-            <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+            <a href="about.php" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
         </div>
     </div>
 
@@ -417,21 +497,21 @@
                         <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
                     </a>
                     <br>
-                    <?php 
-                        if($contact_r['pn2'] != ''){
-                            echo <<<data
+                    <?php
+                    if ($contact_r['pn2'] != '') {
+                        echo <<<data
                                 <a href="tell: +$contact_r[pn2]" class="d-inline-block text-decoration-none text-dark">
                                     <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
                                 </a>
                             data;
-                        }
+                    }
                     ?>
                 </div>
                 <div class="bg-white p-4 rounded mb-4">
                     <h5>Follow us</h5>
-                    <?php 
-                        if($contact_r['tw'] != ''){
-                            echo <<<data
+                    <?php
+                    if ($contact_r['tw'] != '') {
+                        echo <<<data
                                 <a href="$contact_r[tw]" class="d-inline-block mb-3">
                                     <span class="badge bg-light text-dark fs-6 p-2">
                                         <i class="bi bi-twitter-x me-1"></i>Twitter
@@ -439,7 +519,7 @@
                                 </a>
                                 <br>
                             data;
-                        }
+                    }
                     ?>
                     <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block mb-3">
                         <span class="badge bg-light text-dark fs-6 p-2">
